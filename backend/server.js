afb74729910486a7c28a1785c7347f871945e826
Leonard Museau <lmuseau@gmail.com>
2020-11-05 18:38:56 -0600

    configured routes for user and exercises

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,18 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { 
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
